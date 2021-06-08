76a74efb5b6aeb8aad31ca9125e8795dc522f07b
Magdiel Marquez Hernandez <magdielikari@gmail.com>
2021-06-07 21:13:54 -0500

    Basic Crud

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -29,5 +32,168 @@ func GetTodos(c *fiber.Ctx) error {
 		"data": fiber.Map{
 			"todos": todos,
 		},
+	})
+}
+
+func CreateTodo(c *fiber.Ctx) error {
+	type Request struct {
+		Title string `json:"title"`
+	}
+
+	var body Request
+	err := c.BodyParser(&body)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+		})
+	}
+
+	todo := &Todo{
+		Id:        len(todos) + 1,
+		Title:     body.Title,
+		Completed: false,
+	}
+
+	todos = append(todos, todo)
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todo": todo,
+		},
+	})
+}
+
+func GetTodo(c *fiber.Ctx) error {
+	// get parameter value
+	paramId := c.Params("id")
+
+	// convert parameter value string to int
+	id, err := strconv.Atoi(paramId)
+
+	// if error in parsing string to int
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse Id",
+		})
+	}
+
+	// find todo and return
+	for _, todo := range todos {
+		if todo.Id == id {
+			return c.Status(fiber.StatusOK).JSON(fiber.Map{
+				"success": true,
+				"data": fiber.Map{
+					"todo": todo,
+				},
+			})
+		}
+	}
+
+	// if todo not available
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"success": false,
+		"message": "Todo not found",
+	})
+}
+
+func UpdateTodo(c *fiber.Ctx) error {
+	// find parameter
+	paramId := c.Params("id")
+
+	// convert parameter string to int
+	id, err := strconv.Atoi(paramId)
+
+	// if parameter cannot parse
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+		})
+	}
+
+	// request structure
+	type Request struct {
+		Title     *string `json:"title"`
+		Completed *bool   `json:"completed"`
+	}
+
+	var body Request
+	err = c.BodyParser(&body)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+		})
+	}
+
+	var todo *Todo
+
+	for _, t := range todos {
+		if t.Id == id {
+			todo = t
+			break
+		}
+	}
+
+	if todo.Id == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "Not found",
+		})
+	}
+
+	if body.Title != nil {
+		todo.Title = *body.Title
+	}
+
+	if body.Completed != nil {
+		todo.Completed = *body.Completed
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todo": todo,
+		},
+	})
+}
+
+func DeleteTodo(c *fiber.Ctx) error {
+	// get param
+	paramId := c.Params("id")
+
+	// convert param string to int
+	id, err := strconv.Atoi(paramId)
+
+	// if parameter cannot parse
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+		})
+	}
+
+	// find and delete todo
+	for i, todo := range todos {
+		if todo.Id == id {
+
+			todos = append(todos[:i], todos[i+1:]...)
+
+			return c.Status(fiber.StatusNoContent).JSON(fiber.Map{
+				"success": true,
+				"message": "Deleted Succesfully",
+			})
+		}
+	}
+
+	// if todo not found
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"success": false,
+		"message": "Todo not found",
 	})
 }
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/mmagdiel/goTodo/routes"
 )
 
 func setupRoutes(app *fiber.App) {
-	// give response when at /
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"success": true,
@@ -15,18 +14,7 @@ func setupRoutes(app *fiber.App) {
 		})
 	})
 
-	// api group
 	api := app.Group("/api")
-
-	// give response when at /api
-	api.Get("", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"success": true,
-			"message": "You are at the api endpoint 😉",
-		})
-	})
-
-	// connect todo routes
 	routes.TodoRoute(api.Group("/todos"))
 }
 
@@ -36,16 +24,8 @@ func main() {
 
 	setupRoutes(app)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"success": true,
-			"message": "You are at the endpoint 😉",
-		})
-	})
-
 	err := app.Listen(":3000")
 	if err != nil {
 		panic(err)
 	}
-
 }
